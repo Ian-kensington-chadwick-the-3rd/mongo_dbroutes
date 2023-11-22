a6f6ea3cb8672298e6103f6f196ba012a746e4c8
Ian Sills <iansills04@gmail.com>
2023-11-21 21:57:40 -0600

    got done with my models you'll know if i screwed up in a couple commits lol

---
+++ models/Thought.js
@@ -0,0 +1,80 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+const reactionSchema = new Schema(
+
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON:{
+            getters: true
+        },
+    }
+);
+
+
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
+
+// * `thoughtText`
+//   * String
+//   * Required
+//   * Must be between 1 and 280 characters
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+
+// * `username` (The user that created this thought)
+//   * String
+//   * Required
+
+// * `reactions` (These are like replies)
+//   * Array of nested documents created with the `reactionSchema`
+
+// **Schema Settings**:
+
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
